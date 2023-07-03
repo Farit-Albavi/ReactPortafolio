688ee88d033002820dc3e2235938e1d08fe67fc1
faritdev <albavi.farit2@gmail.com>
2023-07-03 04:13:54 -0400

    s

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,12 +5,12 @@ function Contact() {
         <section className='container mt-20 flex flex-col-reverse sm:flex-row'>
 
             <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d757.0069129128622!2d-57.565792712318256!3d-25.506646393499484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjXCsDMwJzI1LjIiUyA1N8KwMzMnNTguNCJX!5e0!3m2!1ses-419!2spy!4v1688369812009!5m2!1ses-419!2spy"
-                className='w-full h-96 sm:h-full'
+                className='w-full h-96 sm:h-full hidden sm:block'
                 allowfullscreen="" loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade">
             </iframe>
 
-            <form className='container p-5 sm:max-w-xl flex flex-col border border-blue-950 rounded m-auto sm:py-40'>
+            <form className='container p-5 sm:max-w-xl flex flex-col border dark:border-blue-950 rounded m-auto sm:py-40'>
 
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
                 <div className="relative">
@@ -21,12 +21,12 @@ function Contact() {
                     </div>
                     <input type="text" id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
                  focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
-                 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" />
+                 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@email.com" />
                 </div>
 
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white mt-5">Your message</label>
                 <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500
-                dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment...">
+                dark:bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a message...">
                 </textarea>
 
                 <button type="button" className="text-blue-700 mt-5 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg 
