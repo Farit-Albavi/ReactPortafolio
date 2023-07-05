8c71aba753914a9c56d5c08a7dcd1f67a06f7214
faritdev <albavi.farit2@gmail.com>
2023-07-05 19:31:50 -0400

    rtesdt

--- src/components/Presentacion.jsx
+++ src/components/Presentacion.jsx
@@ -3,7 +3,7 @@ import 'animate.css';
 
 export default function Presentacion() {
     return (
-        <div className='container flex flex-col items-center sm:block mt-14 xl:pt-44 animate__animated animate__bounceInLeft w-full xl:h-screen'>
+        <div className='container flex flex-col items-center sm:block mt-14 xl:pt-44 animate__animated animate__bounceInLeft w-full h-screen'>
             {/* 
                 agregue clases para que en movile se centre el texto de la presentacion, en caso de que no me guste deberia quitar las siguientes clases:
                     text-center sm:text-left
@@ -22,7 +22,7 @@ export default function Presentacion() {
                     a Front end Devloper
                 </h1>
 
-                <div className='sm:max-w-xl sm:mt-8 mt-1'>
+                <div className='sm:max-w-xl lg:mt-8 mt-1'>
                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-left">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum vel quod rem labore porro, nulla placeat sapiente cupiditate
                         ratione accusantium dolorem velit, ipsa pariatur,quam dolor recusandae ut. Est cupiditate incidunt aut vel, explicabo quas.
@@ -40,10 +40,10 @@ export default function Presentacion() {
             </section>
 
             <aside className='flex flex-col sm:items-end  p-4 sm:p-0 animate__bounceInRight animate__animated'>
-                <h1 className='font-bold text-5xl md:text-6xl  sm:text-end '>My name is ...</h1>
+                <h1 className='font-bold text-2xl md:text-6xl text-end '>My name is ...</h1>
 
-                <div className='sm:max-w-xl mt-8'>
-                    <p className="font-normal text-gray-700 dark:text-gray-400 text-start sm:text-end">
+                <div className='sm:max-w-xl lg:mt-8 mt-1'>
+                    <p className="font-normal text-gray-700 dark:text-gray-400 text-end">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae provident cupiditate consectetur temporibus perferendis quam ipsa
                         explicabo rem placeat aliquam id repudiandae fuga ex, earum amet odio natus! Maiores, fugit!
                     </p>
