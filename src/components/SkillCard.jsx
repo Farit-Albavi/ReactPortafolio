7dcd6d543f67ec660283c17462232de1761c9dec
faritdev <albavi.farit2@gmail.com>
2023-07-10 00:41:50 -0400

    SC

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function SkillCard({ title, children }) {
     return (
-        <div className=" flex flex-col items-center justify-center w-40 h-40 max-w-sm text-gray-700">
+        <div className=" flex flex-col items-center justify-center w-28 h-28 sm:w-40 sm:h-40 sm:max-w-sm text-gray-700">
             {children}
             <div className="font-semibold text-xl mt-1 dark:text-white">
                 {title}
